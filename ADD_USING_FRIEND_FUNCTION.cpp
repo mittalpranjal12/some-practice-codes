df44c276df1c338ffbe5ebe9a34fc488aa947bd1
pranjal mittal <pranjalmittal61117@gmail.com>
2020-05-02 17:42:27 +0530

    Add files via upload

---
+++ ADD_USING_FRIEND_FUNCTION.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+
+using namespace std;
+
+class B; //declare class B
+
+class A {
+
+private:
+    int a;
+
+public:
+    //constructor
+    A(int a) {
+        this->a = a;
+    }
+
+    friend int max(A a, B b);
+};
+
+class B {
+
+private:
+    int b;
+
+public:
+    //constructor
+    B(int b) {
+        this->b = b;
+    }
+
+    friend int max(A a, B b);
+};
+
+int max(A a, B b) {
+    return (a.a > b.b ? a.a : b.b);
+}
+
+int main() {
+	int m,n;
+	std::cout<<"Enter first number: ";
+	std::cin>>m;
+	std::cout<<"\n Enter second number: ";
+    std::cin>>n;
+	A a(m);
+    B b(n);
+    cout << "Greatest is : " << max(a, b);
+    return 0;
+}
